8e767c85b92661b62b6b8e5e64211984b7b6b1be
Zeke <67399106+XerckM@users.noreply.github.com>
2022-05-03 09:14:22 -0400

    added files

---
+++ app/src/main/java/com/example/tutoring_app_project/student/StudentActivityContent.java
@@ -0,0 +1,25 @@
+package com.example.tutoring_app_project.student;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.tutoring_app_project.R;
+
+public class StudentActivityContent extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_student_activity_content, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/tutoring_app_project/student/StudentViewAppt.java
@@ -0,0 +1,29 @@
+package com.example.tutoring_app_project.student;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tutoring_app_project.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StudentViewAppt#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StudentViewAppt extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_student_view_appt, container, false);
+        return view;
+    }
+}
