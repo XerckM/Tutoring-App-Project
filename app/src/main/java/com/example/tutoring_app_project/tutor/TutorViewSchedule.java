0dc05007cd75cee9282829c8ca9752e9876e8535
nmaruf1 <Brick@995529>
2022-05-03 18:09:27 -0400

    finish the layout for tutors

--- app/src/main/java/com/example/tutoring_app_project/tutor/TutorViewSchedule.java
+++ app/src/main/java/com/example/tutoring_app_project/tutor/TutorViewSchedule.java
@@ -17,6 +17,8 @@ import com.example.tutoring_app_project.R;
  */
 public class TutorViewSchedule extends Fragment {
 
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
